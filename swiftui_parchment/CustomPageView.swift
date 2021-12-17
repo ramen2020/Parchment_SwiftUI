19f097147c024f111a261450b9a8cb67a21c9743
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-18 07:45:52 +0900

    add custom parchment file

---
+++ swiftui_parchment/CustomPageView.swift
@@ -0,0 +1,14 @@
+//
+//  CustomPageView.swift
+//  swiftui_parchment
+//
+//  Created by 宮本光直 on 2021/12/17.
+//
+
+import SwiftUI
+import Parchment
+
+@available(iOS 13.0, *)
+public struct CustomPageView<Item: PagingItem, Page: View>: View {
+
+}
