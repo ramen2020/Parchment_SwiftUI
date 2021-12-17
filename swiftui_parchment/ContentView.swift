df420f7c00905f22ac8e35d14b8ee633f38bf600
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-17 21:10:11 +0900

    add normal parchment

--- swiftui_parchment/ContentView.swift
+++ swiftui_parchment/ContentView.swift
@@ -5,14 +5,38 @@
 //  Created by 宮本光直 on 2021/12/17.
 //
 
+import Parchment
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                NavigationLink("NormalView", destination: NormalView())
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            navigationBarTitle("Parchment test")
+        }
     }
 }
+
+struct NormalView: View {
+    let items = [
+        PagingIndexItem(index: 0, title: "View 0"),
+        PagingIndexItem(index: 1, title: "View 1"),
+        PagingIndexItem(index: 2, title: "View 2"),
+    ]
+
+    var body: some View {
+        PageView(items: items) { item in
+            Text(item.title)
+                .font(.largeTitle)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
