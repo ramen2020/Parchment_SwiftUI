1fb89e4a607b9a71f746e054e6092fc19d38e223
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-17 21:01:12 +0900

    init

---
+++ swiftui_parchment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui_parchment
+//
+//  Created by 宮本光直 on 2021/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui_parchment/swiftui_parchmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_parchmentApp.swift
+//  swiftui_parchment
+//
+//  Created by 宮本光直 on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_parchmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
